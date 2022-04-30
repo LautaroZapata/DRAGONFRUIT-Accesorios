aea173db418d025830abed6f4226f6e68377352e
LautaroZapata <oratualzc@gmail.com>
2022-04-30 04:06:44 -0300

    UI example

---
+++ js/revealIndex.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal();
+    sr.reveal('.divCardsAccesoriosIg',{
+        duration: 2000,
+        origin: 'bottom',
+        distance: '1000px',
+    });
+    sr.reveal('.divrevealIndex',{
+        duration: 2000,
+        origin: 'bottom',
+        distance: '400px'
+    });
